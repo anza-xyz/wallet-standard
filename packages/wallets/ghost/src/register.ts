d0e27349e00b56bd7f554781a31f823a08fcfa1c
Jordan Sexton <jordan@jordansexton.com>
2022-10-23 22:49:20 -0500

    add setup to wallets

--- packages/wallets/ghost/src/register.ts
+++ packages/wallets/ghost/src/register.ts
@@ -1,12 +1,10 @@
-import type { WalletsWindow } from '@wallet-standard/base';
+import { setupWindowNavigatorWallets } from './setup.js';
 import { GhostWallet } from './wallet.js';
 import type { Ghost } from './window.js';
 
-declare const window: WalletsWindow;
-
 export function register(ghost: Ghost): void {
     try {
-        (window.navigator.wallets ||= []).push(({ register }) => register(new GhostWallet(ghost)));
+        setupWindowNavigatorWallets(({ register }) => register(new GhostWallet(ghost)));
     } catch (error) {
         console.error(error);
     }
