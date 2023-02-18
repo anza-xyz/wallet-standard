96983db93f701b31b9b8da3d5c5f14bc46910649
Jordan Sexton <jordan@jordansexton.com>
2023-02-18 15:38:48 -0600

    add signIn feature

---
+++ packages/core/features/src/signIn.ts
@@ -0,0 +1,79 @@
+import type { IdentifierString, WalletAccount } from '@wallet-standard/base';
+
+/** Name of the feature. */
+export const SolanaSignIn = 'solana:signIn';
+
+/** TODO: docs */
+export type SolanaSignInFeature = {
+    /** Name of the feature. */
+    readonly [SolanaSignIn]: {
+        /** Version of the feature API. */
+        readonly version: SolanaSignInVersion;
+
+        /** Sign In With Solana (based on https://github.com/ChainAgnostic/CAIPs/blob/master/CAIPs/caip-122.md). */
+        readonly signIn: SolanaSignInMethod;
+    };
+};
+
+/** TODO: docs */
+export type SolanaSignInVersion = '1.0.0';
+
+/** TODO: docs */
+export type SolanaSignInMethod = (...inputs: readonly SolanaSignInInput[]) => Promise<readonly SolanaSignInOutput[]>;
+
+/** Input for signing in. */
+export interface SolanaSignInInput {
+    /** EIP-4361 Domain. */
+    readonly domain: string;
+
+    /**
+     * Optional account to use. If not provided, the wallet must determine the account to use.
+     * Used to determine EIP-4361 Address.
+     */
+    readonly account?: WalletAccount;
+
+    /** Optional EIP-4361 Statement. */
+    readonly statement?: string;
+
+    /** EIP-4361 URI. */
+    readonly uri: string;
+
+    /** EIP-4361 Version. */
+    readonly version: '1';
+
+    /**
+     * Chain, as defined by the Wallet Standard.
+     * Used to determine EIP-4361 Chain ID.
+     */
+    readonly chain: IdentifierString;
+
+    /** EIP-4361 Nonce. */
+    readonly nonce: string;
+
+    /** EIP-4361 Issued At. */
+    readonly issuedAt: string;
+
+    /** EIP-4361 Expiration Time. */
+    readonly expirationTime?: string;
+
+    /** EIP-4361 Not Before. */
+    readonly notBefore?: string;
+
+    /** EIP-4361 Request ID. */
+    readonly requestId?: string;
+
+    /** EIP-4361 Resources. */
+    readonly resources?: readonly string[];
+}
+
+/** Output of signing in. */
+export interface SolanaSignInOutput {
+    /** TODO: docs */
+    readonly account: WalletAccount;
+
+    /** TODO: docs */
+    readonly signedMessage: Uint8Array;
+
+    /** TODO: docs */
+    readonly signature: Uint8Array;
+}
