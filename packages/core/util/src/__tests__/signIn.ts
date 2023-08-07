5a81802f31aa8b7f881da7e4727bcd2222a6a53e
Jordan Sexton <jordan@jordansexton.com>
2023-08-06 20:44:06 -0600

    refactor tests

--- packages/core/util/src/__tests__/signIn.ts
+++ packages/core/util/src/__tests__/signIn.ts
@@ -1,41 +1,89 @@
-import { createSignInMessageText, parseSignInMessageText } from '../signIn.js';
+import {
+    createSignInMessage,
+    createSignInMessageText,
+    deriveSignInMessage,
+    deriveSignInMessageText,
+    parseSignInMessage,
+    parseSignInMessageText,
+    verifySignIn,
+} from '../signIn.js';
 
-const signInMessage = {
-    withDomainAndAddress: {
-        object: {
+const signInMessageTests = {
+    'with `domain` and `address`': {
+        parsed: {
             domain: 'solana.com',
             address: 'A',
         },
         text: 'solana.com wants you to sign in with your Solana account:\nA',
     },
-    withStatement: {
-        object: {
+    'with `statement`': {
+        parsed: {
             domain: 'solana.com',
             address: 'A',
             statement: 'S',
         },
         text: 'solana.com wants you to sign in with your Solana account:\nA\n\nS',
     },
+    'with multi-line `statement`': {
+        parsed: {
+            domain: 'solana.com',
+            address: 'A',
+            statement: 'S\n\nS',
+        },
+        text: 'solana.com wants you to sign in with your Solana account:\nA\n\nS\n\nS',
+    },
+    'with fields': {
+        parsed: {
+            domain: 'solana.com',
+            address: 'A',
+            uri: 'https://solana.com',
+        },
+        text: 'solana.com wants you to sign in with your Solana account:\nA\n\nURI: https://solana.com',
+    },
+    'with `statement` and fields': {
+        parsed: {
+            domain: 'solana.com',
+            address: 'A',
+            statement: 'S',
+            uri: 'https://solana.com',
+        },
+        text: 'solana.com wants you to sign in with your Solana account:\nA\n\nS\n\nURI: https://solana.com',
+    },
+    'with multi-line `statement` and fields': {
+        parsed: {
+            domain: 'solana.com',
+            address: 'A',
+            statement: 'S\n\nS',
+            uri: 'https://solana.com',
+        },
+        text: 'solana.com wants you to sign in with your Solana account:\nA\n\nS\n\nS\n\nURI: https://solana.com',
+    },
 };
 
-describe('createSignInMessageText()', () => {
-    it('creates sign in message with `domain` and `address`', () => {
-        const text = createSignInMessageText(signInMessage.withDomainAndAddress.object);
-        expect(text).toBe(signInMessage.withDomainAndAddress.text);
-    });
-    it('creates sign in message with `statement`', () => {
-        const text = createSignInMessageText(signInMessage.withStatement.object);
-        expect(text).toBe(signInMessage.withStatement.text);
-    });
-});
+describe.skip('verifySignIn()', () => {});
+
+describe.skip('deriveSignInMessage()', () => {});
+
+describe.skip('deriveSignInMessageText()', () => {});
+
+describe.skip('parseSignInMessage()', () => {});
 
 describe('parseSignInMessageText()', () => {
-    it('parses sign in message with `domain` and `address`', () => {
-        const parsed = parseSignInMessageText(signInMessage.withDomainAndAddress.text);
-        expect(parsed).toEqual(signInMessage.withDomainAndAddress.object);
-    });
-    it('parses sign in message with `statement`', () => {
-        const parsed = parseSignInMessageText(signInMessage.withStatement.text);
-        expect(parsed).toEqual(signInMessage.withStatement.object);
-    });
+    for (const [name, test] of Object.entries(signInMessageTests)) {
+        it(name, () => {
+            const parsed = parseSignInMessageText(test.text);
+            expect(parsed).toEqual(test.parsed);
+        });
+    }
+});
+
+describe.skip('createSignInMessage()', () => {});
+
+describe('createSignInMessageText()', () => {
+    for (const [name, test] of Object.entries(signInMessageTests)) {
+        it(name, () => {
+            const text = createSignInMessageText(test.parsed);
+            expect(text).toBe(test.text);
+        });
+    }
 });
