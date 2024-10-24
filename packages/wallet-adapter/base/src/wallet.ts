4179c5221162e29d3b15c85a2519b0e4efd28b33
AtlasTylo <165769083+AtlasTylo@users.noreply.github.com>
2024-10-24 16:31:20 +0200

    Add StandardEventsFeature as a return type to features()
    Just added StandardEventsFeature as a return type as it was missing and my IDE did not like it.

--- packages/wallet-adapter/base/src/wallet.ts
+++ packages/wallet-adapter/base/src/wallet.ts
@@ -106,6 +106,7 @@ export class SolanaWalletAdapterWallet implements Wallet {
 
     get features(): StandardConnectFeature &
         StandardDisconnectFeature &
+        StandardEventsFeature &
         SolanaSignAndSendTransactionFeature &
         Partial<SolanaSignTransactionFeature & SolanaSignMessageFeature & SolanaSignInFeature> {
         const features: StandardConnectFeature &
